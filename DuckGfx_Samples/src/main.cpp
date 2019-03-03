262b55fb4003d85dd1ee0da0506c481be20a9152
Cole <cfingram0@gmail.com>
2019-03-02 20:38:04 -0800

    some more fix-up

--- DuckGfx_Samples/src/main.cpp
+++ DuckGfx_Samples/src/main.cpp
@@ -8,8 +8,8 @@
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
-WCHAR * szTitle = L"DuckGfx Samples";                // The title bar text
-WCHAR * szWindowClass = L"Window";            // the main window class name
+char * szTitle = "DuckGfx Samples";                // The title bar text
+char * szWindowClass = "Window";            // the main window class name
 bool running = true;
                                                 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -74,7 +74,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-  WNDCLASSEXW wcex;
+  WNDCLASSEX wcex;
 
 
   ZeroMemory(&wcex, sizeof(WNDCLASSEX));
@@ -89,7 +89,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
   wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
   wcex.lpszClassName = szWindowClass;
 
-  return RegisterClassExW(&wcex);
+  return RegisterClassEx(&wcex);
 }
 
 //
@@ -106,7 +106,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, HWND * outWnd)
 {
   hInst = hInstance; // Store instance handle in our global variable
 
-  HWND hWnd = CreateWindowW(szWindowClass, 
+  HWND hWnd = CreateWindow(szWindowClass, 
                             szTitle, 
                             WS_OVERLAPPEDWINDOW, 
                             CW_USEDEFAULT, 
